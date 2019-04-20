db054a4a7b300bed8591b6d42c4997532f8a2469
William Kronmiller <wrk961@gmail.com>
2019-04-20 07:26:04 -0400

    WIP

---
+++ SnowRunAnalyzer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowRunAnalyzer
+//
+//  Created by William Kronmiller on 3/3/19.
+//  Copyright © 2019 William Rory Kronmiller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
